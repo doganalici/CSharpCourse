c6770e8d6c0e4c720baaa5866ff400d51de3970b
Dogan Alici <doganalici2144@gmail.com>
2025-07-06 13:30:37 +0300

    Update

---
+++ Arrays/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+
+            string[] students2 = {"Engin","Derin","Salih"};
+
+            string[] students3 = new string[]{"Engin","Derin","Salih"};
+
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\n***********\n");
+
+            foreach (string student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\n***********\n");
+
+            foreach (string student in students3)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine("\n***********\n");
+
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("----------");
+            }
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoop();
+
+            //WhileLoop();
+
+            //DoWhileLoop();
+
+            ForEachLoop();
+        }
+
+        private static void ForEachLoop()
+        {
+            string[] strudents = new string[3] { "Engin", "Derin", "Salih" };
+            foreach (string student in strudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        private static void DoWhileLoop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number > 0);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine($"Number is {number}");
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}
