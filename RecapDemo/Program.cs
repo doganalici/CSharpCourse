e1de3bb83e18cfd36d1f39c7c5ab5deb66d12e8f
Dogan Alici <doganalici2144@gmail.com>
2025-09-27 16:17:27 +0300

    Update

---
+++ RecapDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger=new SmsLoger();
+            customerManager.Add();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer Added!");
+        }
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+    class FileLoger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+    class SmsLoger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to SMS!");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+}
