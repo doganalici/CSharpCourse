c084f443155932672e6cec0c986dcfec0c3fd843
Dogan Alici <doganalici2144@gmail.com>
2025-09-27 12:50:11 +0300

    Update

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+
+            MySql mySql = new MySql();
+            mySql.Add();
+
+        }
+    }
+}
+class Database
+{
+    public virtual void Add()
+    {
+        Console.WriteLine("Added by default");
+    }
+    public virtual void Delete()
+    {
+        Console.WriteLine("Deleted by default");
+    }
+}
+
+class SqlServer : Database
+{
+    public override void Add()
+    {
+        Console.WriteLine("Added by Sql Code");
+    }
+    public override void Delete()
+    {
+        Console.WriteLine("Deleted by Sql Code");
+    }
+}
+
+class MySql : Database
+{
+    public override void Add()
+    {
+        Console.WriteLine("Added by MySql Code");
+    }
+    public override void Delete()
+    {
+        Console.WriteLine("Deleted by MySql Code");
+    }
+}
