b5a9f114174389a3fe2921e79889af77f74f7b3e
Dogan Alici <doganalici2144@gmail.com>
2025-10-21 20:10:55 +0300

    Update

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer
+    {
+        protected int Id { get; set; }
+        public void Save()
+        {
+            int id;
+        }
+        public void Delete()
+        {
+
+        }
+    }
+    class Student : Customer
+    {
+        public void Save2()
+        {
+
+        }
+    }
+    internal class Course //internal classı bağlı olduğu proje içinde referans ihtiyacı olmadan kullanılabilir
+    {
+        public string Name { get; set; }
+    }
+    public class Course2 //referans verildiği zaman herhangi bir projede de kullanılabilir
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+
+namespace AccessModifiersDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Course2 course = new Course2();
+        }
+    }
+}
