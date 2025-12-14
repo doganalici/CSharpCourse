09a1beef434c5d7ae295f38f73084760c32ec503
Dogan Alici <doganalici2144@gmail.com>
2025-12-14 07:26:39 +0300

    Update

---
+++ Generics/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Product
+    {
+
+    }
+
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+
+    class Customer
+    {
+
+    }
+
+    interface ICustomerDal : IRepository<Customer>
+    {
+
+    }
+
+    interface IRepository<T>
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T product);
+        void Delete(T product);
+        void Update(T product);
+    }
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
